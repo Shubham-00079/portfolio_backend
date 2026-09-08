27fedd2e646495dd6f986c8a3861bb91043c5474
Shubham-00079 <shubham93014@gmail.com>
2026-09-08 13:34:33 +0530

    Update CORS allowed origins for production

--- src/main/java/com/shubham/portfolio/config/CorsConfig.java
+++ src/main/java/com/shubham/portfolio/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://portfolio-fronted-diya.onrender.com");
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
